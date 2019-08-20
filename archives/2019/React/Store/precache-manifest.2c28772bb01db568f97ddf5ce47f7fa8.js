d6258922b5a8f01b57352495f8754438d14dccdf
coolivan <245333700@qq.com>
2019-08-20 16:15:18 +0800

    更新了路由basename

---
+++ archives/2019/React/Store/precache-manifest.2c28772bb01db568f97ddf5ce47f7fa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16acf519b33d0023fa3a8ac3515d02bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d99c2054a6e33c08773a",
+    "url": "./static/css/main.a51354e5.chunk.css"
+  },
+  {
+    "revision": "806855260c78b5406b0f",
+    "url": "./static/js/2.c5098e73.chunk.js"
+  },
+  {
+    "revision": "d99c2054a6e33c08773a",
+    "url": "./static/js/main.1cc4aa62.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
