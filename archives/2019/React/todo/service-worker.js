6b03fc68f8e47f3af2fe35875887bccede8e2874
coolivan <245333700@qq.com>
2019-09-27 10:57:30 +0800

    update todo

--- archives/2019/React/todo/service-worker.js
+++ archives/2019/React/todo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ea2409f900ffe124aac58609491d35e.js"
+  "./precache-manifest.7053e9c406e7cf284d3ccaf83f2342f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
